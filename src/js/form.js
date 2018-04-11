e301d4ca6928e03d1115944b8b2800e5db965441
Daria Kalinina <lynxx91@mail.ru>
2018-04-11 21:11:50 +0300

    change js to JQuery

--- src/js/form.js
+++ src/js/form.js
@@ -1,38 +1,44 @@
 //скрипт
 'use srtict';
 
-let firstNumber = null;
-let secondNumber = null;
-let totalSum = null;
+let sumNumberFirst = $('.sum__number--first');
+let sumNumberSecond = $('.sum__number--second');
 
-let sumNumberFirst = document.querySelector('.sum__number--first');
-let sumNumberSecond = document.querySelector('.sum__number--second');
-console.log(sumNumberFirst.textContent);
-console.log(sumNumberSecond.textContent);
+let strokeFirst = $('.calc__stroke-img--first');
+let strokeSecond = $('.calc__stroke-img--second');
 
-let strokeFirst = document.querySelector('.calc__stroke-img--first');
-let strokeSecond = document.querySelector('.calc__stroke-img--second');
+let inputFirst = $('.input__number--firstNum');
+let inputSecond = $('.input__number--secondNum');
+let inputSum = $('.input__number--sum');
 
-firstNumber = randomInteger(6, 9);
-secondNumber = chooseSecondNumber();
-sumNumberFirst.textContent = firstNumber;
-sumNumberSecond.textContent = secondNumber;
+
+let firstNumber = randomInteger(6, 9);
+let secondNumber = chooseSecondNumber();
+
+//присваивание значений в формуле
+sumNumberFirst.text(firstNumber);
+sumNumberSecond.text(secondNumber);
+
 drawStroke(firstNumber, strokeFirst);
+inputFirst.css('display', 'block')
 
 drawStroke(secondNumber, strokeSecond);
 
 
-totalSum = firstNumber + secondNumber;
+let totalSum = firstNumber + secondNumber;
 console.log('firstNumber = ', firstNumber);
 console.log('secondNumber = ', secondNumber);
 console.log('totalSum = ', totalSum);
 
 //отрисовка стрелок
 function drawStroke(number, elem) {
+  let weightValue = number * 39;
+  let heightValue = number * 10;
+  let urlValue = 'url(../img/stroke-' + number + '.svg) center center no-repeat';
   //вычисление ширины и высоты блока, числа взяты из пропорции у шкале из макета
-  elem.style.background = "url(../img/stroke-"+number+".svg) center center no-repeat";
-  elem.style.width = (number*39)+"px";
-  elem.style.height = (number*10)+"px";
+  elem.css({ background: urlValue });
+  elem.css({ width: weightValue + 'px' });
+  elem.css({ height: heightValue + 'px' });
 
   return true;
 }
@@ -60,3 +66,79 @@ function randomInteger(min, max) {
 
   return rand;
 }
+
+
+
+
+
+
+// let iFirst = $('.input__number--firstNum');
+// let iSecond = $('.input__number--SecondNum');
+// let iSum = $('.input__number--sum');
+// let input = $('.input__number');
+//
+// // функция проверяет длину введенного текста
+// function validateLength(elem, length) {
+//   if(elem.val().length >= length) {
+//     return true;
+//   }
+//   return false;
+// }
+//
+// // проверка на ввод чисел (ошибка, если вводим не число)
+// input.on('keypress', function(e) {
+//   var valNum = e.originalEvent.key;
+//   if ( parseInt(valNum) !== parseInt(valNum)) {
+//     input.addClass('error-value');
+//     input.addClass('error-value');
+//     return false;
+//     } else {
+//     inputNumber.removeClass('error');
+//     return true;
+//   }
+// });
+
+
+
+
+// проверка длину содержимого в input
+// $(document).ready(function(){
+//
+//   $(document).on('change','.card__person',function(){
+//     var isLenght = validateLength(inputPerson, 4);
+//     if(isLenght) {
+//       inputPerson.removeClass('error');
+//       return true;
+//       }
+//     inputPerson.addClass('error');
+//     return false;
+//   });
+//
+//
+// });
+
+// функция валидации формы - проверяет, чтобы все поля были введены
+// function validateForm() {
+//   var person = inputPerson.val();
+//   var cvc = inputCVV.val();
+//   for (var i = 0; i < inputNumber.length; i++) {
+//     var num = inputNumber[i].value;
+//     if(num.length<4) return false;
+//   }
+//   if(person.length<4) return false;
+//   if(cvc.length<3) return false;
+//   return true;
+// }
+
+// отправка формы, если все корректно введено
+// $('#submit').on('click', function(e) {
+//   var isValidForm = validateForm();
+//   if (isValidForm) {
+//     $('#form').on('submit', function (e) {
+//       console.log('ok');
+//     });
+//   } else{
+//     e.preventDefault();
+//     console.log('no ok');
+//   }
+// });
